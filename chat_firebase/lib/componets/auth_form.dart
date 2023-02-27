0831f1335aa5153d0fa88d200305189df9ca304b
Yures1lva <yure.siilva9@gmail.com>
2023-02-27 16:34:27 -0300

    trabalhando no estado do card

--- chat_firebase/lib/componets/auth_form.dart
+++ chat_firebase/lib/componets/auth_form.dart
@@ -1,9 +1,22 @@
+import 'package:chat_firebase/models/auth_form_data.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class AuthForm extends StatelessWidget {
+class AuthForm extends StatefulWidget {
   const AuthForm({super.key});
+
+  @override
+  State<AuthForm> createState() => _AuthFormState();
+}
+
+class _AuthFormState extends State<AuthForm> {
+  final _formkey = GlobalKey<FormState>();
+  final _formData = AuthFormData();
+
+  void _submit() {
+    _formkey.currentState?.validate();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,27 +25,48 @@ class AuthForm extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.all(16),
         child: Form(
+            key: _formkey,
             child: Column(
-          children: [
-            TextFormField(
-              decoration: InputDecoration(labelText: 'Name'),
-            ),
-            TextFormField(
-              decoration: InputDecoration(labelText: 'E-mail'),
-            ),
-            TextFormField(
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Password',
-              ),
-            ),
-            SizedBox(
-              height: 12,
-            ),
-            ElevatedButton(onPressed: () {}, child: Text("Entrar")),
-            TextButton(onPressed: () {}, child: Text("Criar uma nova conta?"))
-          ],
-        )),
+              children: [
+                if (_formData.isSingup)
+                  TextFormField(
+                    key: ValueKey("name"),
+                    initialValue: _formData.name,
+                    onChanged: (name) => _formData.name = name,
+                    decoration: InputDecoration(labelText: 'Name'),
+                  ),
+                TextFormField(
+                  key: ValueKey("email"),
+                  initialValue: _formData.email,
+                  onChanged: (email) => _formData.email = email,
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                ),
+                TextFormField(
+                  key: ValueKey("password"),
+                  initialValue: _formData.password,
+                  onChanged: (password) => _formData.password = password,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                ElevatedButton(
+                    onPressed: _submit,
+                    child: Text(_formData.isLogin ? "Entrar" : "Cadastrar")),
+                TextButton(
+                    child: Text(_formData.isLogin
+                        ? "Criar uma nova conta?"
+                        : 'Já possui conta?'),
+                    onPressed: () {
+                      setState(() {
+                        _formData.toggleAuthMode();
+                      });
+                    })
+              ],
+            )),
       ),
     );
   }
